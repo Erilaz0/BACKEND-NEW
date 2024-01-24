ce1c94b76a0ce7da016388b8de9257793779eebf
diego <alonsoalonsl431432@gmail.com>
2024-01-24 18:17:12 -0300

    Final Detailsss

--- src/controllers/viewsController/loginViewController.js
+++ src/controllers/viewsController/loginViewController.js
@@ -1,7 +1,7 @@
 async function loginView( req , res ){
     let getToken = req.cookies.token
     if(getToken){
-        res.setHeader("location","http://localhost:8080/api/products")
+        
         res.status(200).redirect("/api/products")
     }else{
 
