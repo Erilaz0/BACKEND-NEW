f0ae728aa69e419a43285512c95abf5a8a3daa51
diego <alonsoalonsl431432@gmail.com>
2024-01-26 15:59:28 -0300

    Final changes

--- src/controllers/productsControllers/deleteProductsController.js
+++ src/controllers/productsControllers/deleteProductsController.js
@@ -9,16 +9,16 @@ async function deleteProduct( req , res ){
     const productDeleted = await productsService.deleteProduct(id)
     if(productDeleted){
 
-           
-      res.status(200).send("product deleted")
       req.logger.info("product deleted")
+      return res.status(200).send("product deleted")
+     
  
 
     }
     else{
-
-      res.status(400).send("Producto id: " + id + "no existe")
       req.logger.info("Producto id: " + id + "no existe")
+      return res.status(400).send("Producto id: " + id + "no existe")
+      
       
 
      
